4b461ecde0207c65fde14df6b2cfcfdf8835a077
José de Senna <josespnt1990@gmail.com>
2026-02-21 16:22:52 -0300

    feat: centraliza validacao de notas e ajusta fluxo principal
    - Extrai metodo ValidarNota para reduzir duplicacao
    - Torna Aprovacao privado
    - Define SolucaoProblema como fluxo padrao

--- Projeto/Src/Aluno.cs
+++ Projeto/Src/Aluno.cs
@@ -11,7 +11,7 @@ class Aluno
 
     public Aluno(string nome, double nota1, double nota2, double nota3)
     {
-        if (nome != null && nome != "")
+        if (NomeValido(nome))
         {
             _nome = nome;
         }
@@ -20,33 +20,12 @@ class Aluno
             _nome = "Aluno";
         }
 
-        if (nota1 >= 0.0 && nota1 <= 30.0)
-        {
-            Nota1 = nota1;
-        }
-        else
-        {
-            Nota1 = 20.0;
-        }
-
-        if (nota2 >= 0.0 && nota2 <= 35.0)
-        {
-            Nota2 = nota2;
-        }
-        else
-        {
-            Nota2 = 20.0;
-        }
-
-        if (nota3 >= 0.0 && nota3 <= 35.0)
-        {
-            Nota3 = nota3;
-        }
-        else
-        {
-            Nota3 = 20.0;
-        }
+        Nota1 = ValidarNota(nota1, 0.0, 30.0);
+        Nota2 = ValidarNota(nota2, 0.0, 35.0);
+        Nota3 = ValidarNota(nota3, 0.0, 35.0);
     }
+
+    public Aluno(string nome) : this(nome, 20.0, 20.0, 20.0) {}
 
     public string Nome
     {
@@ -57,21 +36,43 @@ class Aluno
 
         set
         {
-        if (value != null && value != "")
+        if (NomeValido(value))
             {
                 _nome = value;
             }
         }
     }
 
-    public Aluno(string nome) : this(nome, 20.0, 20.0, 20.0) {}
+    private static bool NomeValido(string nome)
+    {
+        if (nome != null && nome != "")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private static double ValidarNota(double nota, double min, double max)
+    {
+        if (nota >= min && nota <= max)
+        {
+            return nota;
+        }
+        else
+        {
+            return 20.0;
+        }
+    }
 
     public double NotaFinal()
     {
         return Nota1 + Nota2 + Nota3;
     }
 
-    public static bool Aprovacao(double nota)
+    private static bool Aprovacao(double nota)
     {
         if (nota >= 60.0)
         {
